bc56f94c31e19345b95edb948c53690d2b4dca34
Ele975 <55561956+Ele975@users.noreply.github.com>
2025-10-20 13:03:48 +0200

    fixed, using openAI, other models bad if small

--- src/models.py
+++ src/models.py
@@ -11,6 +11,7 @@ import torch
 from transformers import pipeline, BitsAndBytesConfig
 from langchain_tavily import TavilySearch
 from langchain_community.embeddings import HuggingFaceEmbeddings
+from langchain_openai import ChatOpenAI
 
 # load environment variables from .env file
 try:
@@ -38,54 +39,73 @@ def get_langsmith():
 
 
 def get_llm_summarization():
-    # model_id = "TinyLlama/TinyLlama-1.1B-Chat-v1.0"
-    model_path = "models_downloaded/hub/models--TinyLlama--TinyLlama-1.1B-Chat-v1.0/snapshots/fe8a4ea1ffedaf415f4da2f062534de366a451e6"
-    tokenizer = AutoTokenizer.from_pretrained(model_path)
+    # # model_id = "TinyLlama/TinyLlama-1.1B-Chat-v1.0"
+    # model_path = "models_downloaded/hub/models--TinyLlama--TinyLlama-1.1B-Chat-v1.0/snapshots/fe8a4ea1ffedaf415f4da2f062534de366a451e6"
+    # tokenizer = AutoTokenizer.from_pretrained(model_path)
     
-    model = AutoModelForCausalLM.from_pretrained(
-        model_path,
-        device_map="auto",
-        dtype=torch.float16  
-    )  
+    # model = AutoModelForCausalLM.from_pretrained(
+    #     model_path,
+    #     device_map="auto",
+    #     dtype=torch.float16  
+    # )  
 
-    summarizer = pipeline(
-    "text-generation",
-    model=model,
-    tokenizer=tokenizer,
-    max_new_tokens=256,
-    temperature=0.0,
-    do_sample=False
+    # summarizer = pipeline(
+    # "text-generation",
+    # model=model,
+    # tokenizer=tokenizer,
+    # max_new_tokens=256,
+    # temperature=0.0,
+    # do_sample=False
+    # )
+
+    # return HuggingFacePipeline(pipeline=summarizer)
+
+    if not os.environ.get("OPENAI_API_KEY"):
+        print("Missing OpenAI key.")
+        sys.exit(1)
+
+    return ChatOpenAI(
+        model = "gpt-4-turbo",
+        temperature = 0.0,
+        max_tokens = 256
     )
-
-    return HuggingFacePipeline(pipeline=summarizer)
     
 def get_llm():
-    token = os.environ.get("HUGGINGFACEHUB_API_TOKEN")
-    if not token:
-        raise ValueError("Missing Hugging Face token. Set HUGGINGFACEHUB_API_TOKEN.")
+    # token = os.environ.get("HUGGINGFACEHUB_API_TOKEN")
+    # if not token:
+    #     raise ValueError("Missing Hugging Face token. Set HUGGINGFACEHUB_API_TOKEN.")
     
-    # model_path = "models_downloaded/hub/models--mistralai--Mistral-7B-Instruct-v0.2/snapshots/63a8b081895390a26e140280378bc85ec8bce07a"
-    model_path = "models_downloaded/hub/models--TinyLlama--TinyLlama-1.1B-Chat-v1.0/snapshots/fe8a4ea1ffedaf415f4da2f062534de366a451e6"
+    # # model_path = "models_downloaded/hub/models--mistralai--Mistral-7B-Instruct-v0.2/snapshots/63a8b081895390a26e140280378bc85ec8bce07a"
+    # model_path = "models_downloaded/hub/models--TinyLlama--TinyLlama-1.1B-Chat-v1.0/snapshots/fe8a4ea1ffedaf415f4da2f062534de366a451e6"
 
-    # model_id = "mistralai/Mistral-7B-Instruct-v0.2"
-    tokenizer = AutoTokenizer.from_pretrained(model_path)
+    # # model_id = "mistralai/Mistral-7B-Instruct-v0.2"
+    # tokenizer = AutoTokenizer.from_pretrained(model_path)
 
-    model = AutoModelForCausalLM.from_pretrained(
-        model_path,
-        device_map="auto",
-        dtype=torch.float16
+    # model = AutoModelForCausalLM.from_pretrained(
+    #     model_path,
+    #     device_map="auto",
+    #     dtype=torch.float16
+    # )
+
+    # generator = pipeline(
+    #     "text-generation",
+    #     model=model,
+    #     tokenizer=tokenizer,
+    #     max_new_tokens=512,
+    #     temperature=0.5
+    # )
+
+    # return HuggingFacePipeline(pipeline=generator)
+
+    if not os.environ.get("OPENAI_API_KEY"):
+        print("Missing OpenAI key.")
+        sys.exit(1)
+
+    return ChatOpenAI(
+        model = "gpt-4-turbo",
+        temperature = 0.0,
+        max_tokens = 256
     )
-
-    generator = pipeline(
-        "text-generation",
-        model=model,
-        tokenizer=tokenizer,
-        max_new_tokens=512,
-        temperature=0.5
-    )
-
-    return HuggingFacePipeline(pipeline=generator)
-
 
 def get_retriever():
     # model_name = "sentence-transformers/all-MiniLM-L6-v2"
